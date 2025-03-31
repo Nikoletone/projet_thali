4f6500a9c256ec3c864f33ae671caf39bcbc5c82
Nikoletone <64850015+Nikoletone@users.noreply.github.com>
2025-03-31 16:22:09 +0200

    etape 2 v4

--- Documents/Cours-S2-N1/projet_java/JavaApplication_1SLAM_PROJET2_THALI_MINI/JavaApplication_1SLAM_PROJET2_THALI_MINI/src/gui/JFrameLesExcursions.java
+++ Documents/Cours-S2-N1/projet_java/JavaApplication_1SLAM_PROJET2_THALI_MINI/JavaApplication_1SLAM_PROJET2_THALI_MINI/src/gui/JFrameLesExcursions.java
@@ -47,7 +47,7 @@ public class JFrameLesExcursions extends javax.swing.JFrame {
         jComboBoxExcursions.setModel(modeleJComboExcursions);
 
     }
-
+  
     // SERVICES
     public void remplirJComBoxExcursions(List<MiniExcursion> desExcursions) {
         modeleJComboExcursions.removeAllElements();
@@ -89,22 +89,30 @@ public class JFrameLesExcursions extends javax.swing.JFrame {
      * Afficher les étapes d'une mini-excursion dans le tableau
      * @param uneExcursion Excursion dont on doit afficher les étapes
      */
-    public void majAffichageEtapes(MiniExcursion uneExcursion) {
+   public void majAffichageEtapes(MiniExcursion uneExcursion) {
         // vider le modèle du composant JTable (effacer les anciennes données)
         modeleJTableEtapes.setRowCount(0);
         // Une ligne de la table est un tableau d'objets
         Object[] rowData = new Object[jTableEtapes.getColumnModel().getColumnCount()];
+        int sommeDurees = 0; // somme des durées d'une excursion
+                
         // Pour chaque étape de l'excursion
         for (Etape uneEtape : uneExcursion.getLesEtapes()) {
             // Remplir le tableau d'objets représentant une ligne de la table avec les données de l'étape
             rowData[0] = String.valueOf(uneEtape.getNumEtape());      // n° de l'étape dans la première colonne
             rowData[1] = uneEtape.getDescription();                   // description de l'étape dans la deuxième colonne
             int mn = uneEtape.getDureePrevue(); 
+            sommeDurees += mn; // ajout minutes
+
             // durée prévue dans la troisième colonne, formattée en heures:minutes:secondes
             rowData[2] = String.format("%1$02dh%2$02dmn", (mn / 60), (mn - (mn / 60) * 60));
             // Ajouter la ligne au modèle
             modeleJTableEtapes.addRow(rowData);
         }
+        
+        // Affichage de la durée totale d'une excursion
+        jLabelDureeTotaleCourante.setText(String.format("%1$02dh%2$02dmn", (sommeDurees / 60), (sommeDurees - (sommeDurees / 60) * 60))); 
+        
     }
 
     /**
@@ -121,6 +129,8 @@ public class JFrameLesExcursions extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTableEtapes = new javax.swing.JTable();
+        jLabelDureeTotale = new javax.swing.JLabel();
+        jLabelDureeTotaleCourante = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Module THALI-MINI");
@@ -151,6 +161,10 @@ public class JFrameLesExcursions extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(jTableEtapes);
 
+        jLabelDureeTotale.setText("Durée totale : ");
+
+        jLabelDureeTotaleCourante.setText("courante");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -159,12 +173,19 @@ public class JFrameLesExcursions extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(58, 58, 58)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 622, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(3, 3, 3)
-                        .addComponent(jComboBoxExcursions, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(jComboBoxExcursions, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(58, 58, 58)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(6, 6, 6)
+                                .addComponent(jLabelDureeTotale, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabelDureeTotaleCourante, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 622, Short.MAX_VALUE))))
                 .addGap(63, 63, 63))
             .addGroup(layout.createSequentialGroup()
                 .addGap(256, 256, 256)
@@ -182,7 +203,11 @@ public class JFrameLesExcursions extends javax.swing.JFrame {
                     .addComponent(jComboBoxExcursions, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(69, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelDureeTotale)
+                    .addComponent(jLabelDureeTotaleCourante))
+                .addContainerGap(47, Short.MAX_VALUE))
         );
 
         pack();
@@ -202,6 +227,8 @@ public class JFrameLesExcursions extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> jComboBoxExcursions;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabelDureeTotale;
+    private javax.swing.JLabel jLabelDureeTotaleCourante;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTableEtapes;
     // End of variables declaration//GEN-END:variables
